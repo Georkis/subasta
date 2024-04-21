30b2c06dd362d3e621b4c8ff5aaa28606fc5c4cf
georkis <georkis.santiesteban@gmail.com>
2024-04-21 16:33:06 -0400

    first commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(
+        UserPasswordHasherInterface $passwordHasher,
+    )
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setRoles(['ROLE_ADMIN'])
+            ->setEmail('georkis.santiesteban@gmail.com')
+            ->setPassword($this->passwordHasher->hashPassword(user: $user, plainPassword: 'ADmin1234'));
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
