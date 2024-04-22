67df51d9aa58c12cf9900d989656904220648773
georkis <georkis.santiesteban@gmail.com>
2024-04-22 12:56:53 -0400

    Agregando ruta y acceso... pagina en desarrollo

--- src/Controller/DashboardAdminController.php
+++ src/Controller/DashboardAdminController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route(path: '/admin')]
 class DashboardAdminController extends AbstractController
 {
-    #[Route('/dashboard/admin', name: 'app_dashboard_admin')]
+    #[Route('/dashboard', name: 'app_dashboard_admin')]
     public function index(): Response
     {
         return $this->render('dashboard_admin/index.html.twig', [
